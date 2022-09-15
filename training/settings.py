43c9ec5f82607ebfb71b6f4e9e13f62ea52ddff9
Victor Caram <victorgcaram@gmail.com>
2022-09-14 22:05:15 -0300

    Configuring Allowed Hosts

--- training/settings.py
+++ training/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-
-ALLOWED_HOSTS = ["localhost.com", "http://127.0.0.1/"]
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
